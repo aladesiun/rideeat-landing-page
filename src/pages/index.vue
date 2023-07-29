91b4ce884f6ce56d8904f314f391c1afe1640562
temitope <aladesiuntope@gmail.com>
2023-07-29 14:14:46 +0100

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="font-bold text-red-500">Hello</div>
-  <p>{{counter}}</p>
-  <button @click="increaseCounter">Increase</button>
+Index page you all
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
     layout:'default'
 })
-import {useIndexStore} from "@/store/index";
-import {storeToRefs} from 'pinia'
-const indexStore = useIndexStore()
-const {increaseCounter} = indexStore
-const {counter} = storeToRefs(indexStore)
 </script>
 
 <style>
